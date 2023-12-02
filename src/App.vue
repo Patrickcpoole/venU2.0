78ecbee292c0be3d2d3e9634da6a816eaa323bd5
patrickpoole <patrickcpoole@gmail.com>
2023-12-01 22:05:44 -0700

    removed imports from app

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,11 @@
 </template>
 
 <script>
-import Vue from 'vue';
-import Quasar, { date } from 'quasar';
-import { mapActions } from 'vuex';
+
+
 
 
 export default {
   name: 'App',
-  mounted() {
-    this.authAction();
-  },
-  methods: {
-    ...mapActions('auth', ['authAction']),
-  },
 };
 </script>
